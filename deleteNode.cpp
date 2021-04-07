19a22e56f02632a109ccf1b9072a7babcf56f95e
Lip <873140708@qq.com>
2021-04-07 17:59:37 +0800

    test

--- deleteNode.cpp
+++ deleteNode.cpp
@@ -9,8 +9,9 @@ struct ListNode {
 输入: head = [4,5,1,9], val = 5
 输出: [4,1,9]
 解释: 给定你链表中值为?5?的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
-
 */
+
+
 class DeleteNode {
 public:
     ListNode* deleteNode(ListNode* head, int val) {
