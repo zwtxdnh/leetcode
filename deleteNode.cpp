f3f01fe11976cb5eed803538f4b89d71e9e58a09
Lip <873140708@qq.com>
2021-04-07 17:16:45 +0800

    first commit

---
+++ deleteNode.cpp
@@ -0,0 +1,15 @@
+
+//Definition for singly-linked list.
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(nullptr) {}
+  };
+
+class Solution {
+public:
+    ListNode* deleteNode(ListNode* head, int val) {
+      
+
+    }
+};
